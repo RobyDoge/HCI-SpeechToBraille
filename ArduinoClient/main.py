2e52376114c1b4af5e1b4d5ae120625904107b84
RobyDoge <robert-ionut.popa@student.unitbv.ro>
2025-01-15 21:40:46 +0200

    Add Flask web server and live message feed to Arduino client

--- ArduinoClient/main.py
+++ ArduinoClient/main.py
@@ -1,8 +1,28 @@
-from send_text import send_text
+from flask import Flask, render_template
+import threading
+
 from server_conection import get_text
+from send_text import send_text
+
+app = Flask(__name__)
+
+messages_list = ["test message"]
+
+@app.route('/')
+def index():
+    return render_template('index.html', messages=messages_list)
+
+def update_messages():
+    def aux():
+        while True:
+            #await getting a text, when u do send them to arduino and update the messages_list
+            pass
+    thread = threading.Thread(target=aux)
+    thread.daemon = True
+    thread.start()
 
 if __name__ == '__main__':
-    text = get_text()
-    send_text(text)
+    update_messages()
+    app.run(debug=True, port=5001)
 
 
